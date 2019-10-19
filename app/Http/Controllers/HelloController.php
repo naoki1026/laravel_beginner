1e7b9900cda5fa1a840dc3670b24357df3a221f3
arakawan <arakawan@pivot.jp>
2019-10-19 17:30:30 +0900

    エラー発生

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,24 +7,11 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
 
-    public function index(){
+    public function index() {
         return <<<EOF
-    
-    <html>
-    <head>
-    <title>Hello/Index</title>
-    <style>
-    body { font-size: 16pt; color: #999;}
-    h1 {font-size: 100pt; test-align:right; color:#eee; margin: -40px; o -50px 0;}
-    </style>
-    </head>
-    <body>
-        <h1>Index</h1>
-        <p>これは、Helloコントローラーのindexアクションです。</p>
-    </body>
-    </html>
-
-    EOF;
-
+        <html>
+        <head?
+        </html>
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,41 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('hello', function(){
-//     return
-//     '<html>
-//     <body>
-//     <h1>Hello</h1>
-//     <p>This is sample page.</p>
-//     </body>
-//     </html>';
-// });
-
-
-
-// Route::get('hello', function () use ($html) {
-//     return $html;
-// });
-
-Route::get('hello/{msg}',function ($msg) {
-
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size:16pt; color:#999;}
-h1 { font-size:100px; text-align:right; color: #eee; margin: -40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <p>{$msg}</p>
-    <p>This is sample page.</p>
-    <p>これは、サンプルで作ったページです。</p>
-</body>
-</html>
-EOF;
-
-return $html;
-});
+Route::get('hello', 'HelloController@index');
