02d6ac976f55865c1381fe1df76574655c68e985
arakawan <arakawan@pivot.jp>
2019-10-20 14:02:38 +0900

    94ページまで

---
+++ resources/views/components/message.blade.php
@@ -0,0 +1,10 @@
+<style>
+.message {border: double 4px #ccc; margin: 10px; padding: 10px; 
+background-color: #fafafa;}
+.msg-title {margin: 10px 20px; color: #999;}
+.msg-content {margin: 10px 20px; color: #aaa; font-size: 12pt;}
+</style>
+<div class="message">
+  <p class="msg-title">{{ $msg_title }}</p>
+  <p class="msg-content">{{ $msg_content }}</p>
+</div>
