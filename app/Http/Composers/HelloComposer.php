e257c360b113647f17d1083673c7516e101622fd
arakawan <arakawan@pivot.jp>
2019-10-20 20:14:05 +0900

    106ページまで

---
+++ app/Http/Composers/HelloComposer.php
@@ -0,0 +1,11 @@
+<?php
+nameSpace App\Http\Composers;
+
+use Illuminate\View\View;
+class HelloComposer {
+
+  public function compose(View $view)
+  {
+    $view->with('view_message', 'this view is "' . $view -> getName() . '"!!');
+  }
+}
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,7 +8,7 @@
 @section('content')
 <p>ここが本文のコンテンツです。</p>
 <p>Controller value<br>'message' = {{ $message }}</p>
-<p>ViewComposer value<br>'view_message' = {{ $view_message }}</p>
+<p>ViewComposer value<br>'view_message' = {{ $message }}</p>
 @endsection
 @section('footer')
 copyright 2017 tuyano.
