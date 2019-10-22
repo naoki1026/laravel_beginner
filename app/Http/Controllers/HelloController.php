3789c1226b289f188fbc31b3ba2c08246fa97ff4
arakawan <arakawan@pivot.jp>
2019-10-22 17:57:57 +0900

    143ページまで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,15 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
 public function index(Request $request) {
-return view('hello.index', ['msg' => 'フォームを入力:']);
+    $validator = Validator::make($request->query(), [
+        'id' => 'required',
+        'pass' => 'required',
+    ]);
+    if ($validator->fails()){
+        $msg = 'クエリーに問題があります。';
+    } else {
+        $msg = 'ID/PASSを受け付けました。フォームを入力ください。';
+    }
+    return view('hello.index', ['msg' => $msg, ]);
+
 }
 
 public function post(HelloRequest $request) {
-return view('hello.index', ['msg' => '正しく入力されました!']);
-
+return view('hello.index', ['msg' => '正しく入力されました。:']);
 }
 }
