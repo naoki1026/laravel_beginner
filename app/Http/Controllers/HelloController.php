bd9b6e654840050e4b738943e6a1d7a66eb2626c
arakawan <arakawan@pivot.jp>
2019-10-22 16:43:38 +0900

    142ページまで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,11 +9,11 @@ use App\Http\Requests\HelloRequest;
 class HelloController extends Controller
 {
 public function index(Request $request) {
-    return view('hello.index', ['msg' => 'フォームを入力:']);
+return view('hello.index', ['msg' => 'フォームを入力:']);
 }
 
 public function post(HelloRequest $request) {
-  return view('hello.index', ['msg' => '正しく入力されました!']);
+return view('hello.index', ['msg' => '正しく入力されました!']);
 
 }
 }
--- app/Http/Requests/HelloRequest.php
+++ app/Http/Requests/HelloRequest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class HelloRequest extends FormRequest
 {
+
     public function authorize()
     {
         if($this->path() == 'hello')
@@ -22,6 +23,15 @@ class HelloRequest extends FormRequest
             'name' => 'required',
             'mail' => 'email',
             'age' => 'numeric|between:0, 150',
+            
         ];
     }
+    public function messages() {
+        return [
+            'name.required' => '名前は必ず入力してください。',
+            'mail.email' => 'メールアドレスは必要です。',
+            'age.numeric' => '年齢を整数で記入ください。',
+            'age.between' => '年齢は0~150の間で入力ください。',
+        ];
+}
 }
