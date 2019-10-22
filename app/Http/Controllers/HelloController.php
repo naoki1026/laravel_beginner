c05918031258dbe2318d5b52f9ca2c22de8d95e7
arakawan <arakawan@pivot.jp>
2019-10-22 16:27:54 +0900

    138ページまで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
@@ -11,13 +12,8 @@ public function index(Request $request) {
     return view('hello.index', ['msg' => 'フォームを入力:']);
 }
 
-public function post(Request $request) {
-    $validate_rule = [
-        'name' => 'required',
-        'mail' => 'email',
-        'age' => 'numeric|between:0,150'
-    ];
-    $this -> validate($request, $validate_rule);
-    return view('hello.index', ['msg' => '正しく入力されました!']);
+public function post(HelloRequest $request) {
+  return view('hello.index', ['msg' => '正しく入力されました!']);
+
 }
 }
