f5fc6aa4eb3ea157afb5707ab850ece712bb00cf
arakawan <arakawan@pivot.jp>
2019-11-02 19:46:12 +0900

    190ページまで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -16,15 +16,21 @@ class HelloController extends Controller
     }
 
     public function post(Request $request) {
-        $validate_rule = [
-            'msg' => 'required',
-        ];
-        $this->validate($request, $validate_rule);
-        $msg = $request->msg;
-        $response = new Response(view('hello.index', ['msg'=>
-        '「' . $msg . '」をクッキーに保存しました。']));
-        $response -> cookie('msg', $msg, 100);
-        return $response;
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
     }
 
+    public function add(Request $request) {
+        return view('hello.add');
+    }
+
+    public function create(Request $request) {
+        $param = [
+            'name' => $request -> name,
+            'mail' => $request -> mail,
+            'age' => $request -> age,
+        ];
+        DB::insert('insert into people(name, mail, age) values (:name, :mail, :age)', $param);
+        return redirect('/hello');
+    }
 }
